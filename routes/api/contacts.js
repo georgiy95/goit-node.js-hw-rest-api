124436564e950774c43de032f1f7c7c9e3ea60be
Georgiy95 <urijpetrovskij@gmail.com>
2023-11-20 09:34:09 +0200

    commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,11 +6,8 @@ const { validateBody, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 router.get("/", ctrl.getAll);
-
 router.get("/:contactId", isValidId, ctrl.getById);
-
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
-
 router.delete("/:contactId", isValidId, ctrl.removeById);
 
 router.put(
