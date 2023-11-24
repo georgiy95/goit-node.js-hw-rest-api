d37aa2e3d648fd5e61c7614cb852dc2b45b02cad
Georgiy95 <urijpetrovskij@gmail.com>
2023-11-24 16:53:05 +0200

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,16 +4,12 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
-
 const { User } = require("../models/user");
-
 const { HttpError, ctrlWrapper } = require("../helpers");
-
 const { SECRET_KEY } = process.env;
-
 const avatarDir = path.join(__dirname, "../", "public", "avatars");
 
-// Registration
+
 const register = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -36,7 +32,6 @@ const register = async (req, res) => {
   });
 };
 
-// Login
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
@@ -61,7 +56,6 @@ const login = async (req, res) => {
   res.json({ token, user: { email, subscription: user.subscription } });
 };
 
-// Current
 const getCurrent = async (req, res) => {
   const { email, subscription } = req.user;
   res.json({
@@ -70,14 +64,12 @@ const getCurrent = async (req, res) => {
   });
 };
 
-// Logout
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
   res.status(204).json("");
 };
 
-// Update subscription type
 const updateSubscription = async (req, res) => {
   console.log(req.user);
 
@@ -90,7 +82,6 @@ const updateSubscription = async (req, res) => {
   res.json({ message: `subscription updated to ${req.body.subscription}` });
 };
 
-// Update avatar
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
 
